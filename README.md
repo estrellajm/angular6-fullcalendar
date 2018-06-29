8ded06081514dd4857330a36b7d0055b4092280e
Jose Estrella <jose.estrella@imploie.com>
2018-06-29 01:37:36 -0600

    init

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
-# Angular6Fullcalendar
+# Angular6-Fullcalendar
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
+This is a simple project that was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7. It contains both Calendar & Scheduler from [FullCalendar](https://fullcalendar.io/) using the [Alpha Release](https://fullcalendar.io/blog/2018/04/alpha-release-jquery-removal)
+
+## Install Dependencies
+
+Run `yarn` or `npm i` to install all the necessary dependancies.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## FullCalendar Scheduler License Information
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+For more information about licensing the `Scheduler`please visit [FullCalendar Scheduler License](https://fullcalendar.io/scheduler/license)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,169 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const FullCalendar: any;
+declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit  {
+  ngOnInit() {
+    this.calendar();
+    this.schedule();
+  }
+
+  calendar() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      now: '2018-04-07',
+      editable: true,
+      aspectRatio: 1.8,
+      scrollTime: '00:00',
+      header: {
+        left: 'promptResource today prev,next',
+        center: 'title',
+        right: 'timelineDay,timelineThreeDays,agendaWeek,month'
+      },
+      customButtons: {
+        promptResource: {
+          text: '+ room',
+          click: function() {
+            var title = prompt('Room name');
+            if (title) {
+              calendar.addResource({ title: title }, true); // true = scroll to new resource
+            }
+          }
+        }
+      },
+      defaultView: 'timelineDay',
+      views: {
+        timelineThreeDays: {
+          type: 'timeline',
+          duration: { days: 3 }
+        }
+      },
+      resourceLabelText: 'Rooms',
+      resourceRender: function(resource, tr) {
+        tr.addEventListener('click', function() {
+          if (confirm('Are you sure you want to delete ' + resource.title + '?')) {
+            calendar.removeResource(resource);
+          }
+        });
+      },
+      height: 'auto',
+      resources: [
+        { id: 'a', title: 'Auditorium A' },
+        { id: 'b', title: 'Auditorium B', eventColor: 'green' },
+        { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
+        { id: 'd', title: 'Auditorium D', children: [
+          { id: 'd1', title: 'Room D1' },
+          { id: 'd2', title: 'Room D2' }
+        ] },
+        { id: 'e', title: 'Auditorium E' },
+        { id: 'f', title: 'Auditorium F', eventColor: 'red' },
+        { id: 'g', title: 'Auditorium G' },
+        { id: 'h', title: 'Auditorium H' },
+        { id: 'i', title: 'Auditorium I' },
+        { id: 'j', title: 'Auditorium J' },
+        { id: 'k', title: 'Auditorium K' },
+        { id: 'l', title: 'Auditorium L' },
+        { id: 'm', title: 'Auditorium M' }
+      ],
+      events: [
+        { id: '1', resourceId: 'b', start: '2018-04-07T02:00:00', end: '2018-04-07T07:00:00', title: 'event 1' },
+        { id: '2', resourceId: 'c', start: '2018-04-07T05:00:00', end: '2018-04-07T22:00:00', title: 'event 2' },
+        { id: '3', resourceId: 'd', start: '2018-04-06', end: '2018-04-08', title: 'event 3' },
+        { id: '4', resourceId: 'e', start: '2018-04-07T03:00:00', end: '2018-04-07T08:00:00', title: 'event 4' },
+        { id: '5', resourceId: 'f', start: '2018-04-07T00:30:00', end: '2018-04-07T02:30:00', title: 'event 5' }
+      ]
+    });
+    calendar.render();
+  }
+  schedule() {
+    var scheduleEl = document.getElementById('schedule');
+    var schedule = new FullCalendar.Calendar(scheduleEl, {
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      defaultDate: '2018-04-12',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectHelper: true,
+      select: function(start, end) {
+        var title = prompt('Event Title:');
+        var eventData;
+        if (title) {
+          eventData = {
+            title: title,
+            start: start,
+            end: end
+          };
+          $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+        }
+        $('#calendar').fullCalendar('unselect');
+      },
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2018-04-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2018-04-07',
+          end: '2018-04-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-04-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-04-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2018-04-11',
+          end: '2018-04-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-04-12T10:30:00',
+          end: '2018-04-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2018-04-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-04-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2018-04-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2018-04-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2018-04-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2018-04-28'
+        }
+      ]
+    });
+    schedule.render();
+  }
 }
